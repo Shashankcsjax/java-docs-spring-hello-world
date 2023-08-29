e716d1791352b8815956aea1c3c247d5f50b4f17
Shashankcsjax <142861535+Shashankcsjax@users.noreply.github.com>
2023-08-29 12:51:32 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,14 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.joda.time.DateTime;
 
 @SpringBootApplication 
 @RestController
 public class DemoApplication extends SpringBootServletInitializer {
-	@GetMapping("/current-time")
+	@RequestMapping("/current-time")
 	public static void main(String[] args) {
         // Create a new DateTime instance
         DateTime currentDateTime = new DateTime();
