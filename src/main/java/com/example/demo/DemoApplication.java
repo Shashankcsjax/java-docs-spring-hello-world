9722eeb6fcb2c8ee0b2e0269d411ee4fda13224a
Shashankcsjax <142861535+Shashankcsjax@users.noreply.github.com>
2023-08-29 12:35:10 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ import org.joda.time.DateTime;
 
 @SpringBootApplication
 public class DemoApplication {
-
+	@GetMapping("/current-time")
 	public static void main(String[] args) {
         // Create a new DateTime instance
         DateTime currentDateTime = new DateTime();
