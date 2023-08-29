059c5d31c738a97fc77ce208c4d7917340ca1cf3
Shashankcsjax <142861535+Shashankcsjax@users.noreply.github.com>
2023-08-29 13:14:27 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,19 +3,20 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.joda.time.DateTime;
 
 @SpringBootApplication 
 @RestController
 public class DemoApplication extends SpringBootServletInitializer {
-	@RequestMapping("/current-time")
-	public static void main(String[] args) {
-        // Create a new DateTime instance
-        DateTime currentDateTime = new DateTime();
-
-        // Print the current date and time
-        System.out.println("Current Date and Time: " + currentDateTime);
+	@GetMapping("/calculate-age")
+	public String calculateAge(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate birthdate) {
+        LocalDate currentDate = LocalDate.now();
+        Period age = Period.between(birthdate, currentDate);
+        
+        return "Age: " + age.getYears();
 	}
 }
