1a9528001563cbc31915bb93771b7ed637759c30
Shashankcsjax <142861535+Shashankcsjax@users.noreply.github.com>
2023-08-29 11:31:36 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.joda.time.DateTime;
 
 @SpringBootApplication
 @RestController
@@ -14,6 +15,10 @@ public class DemoApplication {
 	}
 
 	@RequestMapping("/")
+	DateTime currentDateTime = new DateTime();
+
+        // Print the current date and time
+        System.out.println("Current Date and Time: " + currentDateTime);
 	String sayHello() {
 		return "Hello Halim how are you?";
 	}
