99cc79da1885c982f6278dc5ee2721ab730e7db3
Shashankcsjax <142861535+Shashankcsjax@users.noreply.github.com>
2023-08-29 13:16:34 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,22 +1,21 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.joda.time.Years;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication 
 @RestController
-public class DemoApplication extends SpringBootServletInitializer {
-	@GetMapping("/calculate-age")
-	public String calculateAge(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate birthdate) {
-        LocalDate currentDate = LocalDate.now();
-        Period age = Period.between(birthdate, currentDate);
+public class DemoApplication {
+
+    @GetMapping("/calculate-age")
+    public String calculateAge(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate birthdate) {
+        LocalDate currentDate = new LocalDate();
+        Years age = Years.yearsBetween(birthdate, currentDate);
         
         return "Age: " + age.getYears();
-	}
+    }
 }
